9ba903b9b3ca7af5ff58aeec9f5046abfcb2a4b8
Roman ASTIE <roman.astie@wanadoo.fr>
2023-11-21 19:29:04 +0100

    parsing

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: gbonnard <gbonnard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 10:45:30 by gbonnard          #+#    #+#             */
-/*   Updated: 2023/11/14 18:32:48 by gbonnard         ###   ########.fr       */
+/*   Updated: 2023/11/21 19:21:51 by rastie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,53 @@
 int	check_wall(char **map)
 {
 	return (0);
+}
+
+
+/* corriger la carte, remplir de mur pour avoir une carte rectangulaire
+   puis parsing
+   */
+int	get_len_max(char **max)
+{
+	int	lenmax;
+
+	lenmax = 2;
+	while (*map)
+	{
+		if (ft_strlen(*map) > lenmax)
+			lenmax = ft_strlen(*map);
+		map++;
+	}
+	return (lenmax);
+}
+
+int	fill_map(char **map)
+{
+	int	lenmax;
+	int	i;
+	char	*temp;
+	char	*fill;
+	
+	lenmax = get_len_max(map);
+	i = 0;
+	while (map[i])
+	{
+		if (ft_strlen(map[i]) < lenmax)
+		{
+			temp = map[i];
+			fill = malloc(lenmax - ft_strlen(map[i]));
+			if (!fill)
+				return (0);	//error malloc;
+			ft_memset(fill, '1', lenmax - ft_strlen(map[i]));
+			map[i] = ft_strjoin(temp, fill);
+			free(temp);
+			free(fill);
+		}
+		i++;
+	}
+	if (lenmax < 3 || i < 3)
+		return (0);	//error map trop petite
+	return (1);
 }
 
 int	parse_map(char **map, t_data *data)
