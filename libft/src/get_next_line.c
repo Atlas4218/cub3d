2ce99a8e79f3ae37ee71b983946343d23ed0a3d9
Roman ASTIE <roman.astie@wanadoo.fr>
2023-12-07 19:48:29 +0100

    gnl

--- libft/src/get_next_line.c
+++ libft/src/get_next_line.c
@@ -6,65 +6,42 @@
 /*   By: rastie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 17:10:25 by rastie            #+#    #+#             */
-/*   Updated: 2023/01/10 14:30:34 by rastie           ###   ########.fr       */
+/*   Updated: 2023/06/19 14:30:16 by rastie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+#include "get_next_line.h"
 
-int	min(int a, int b)
+int	ft_read_trunc(int fd, char *buff, int size)
 {
-	if (a < b)
-		return (a);
-	return (b);
-}
-
-char	*gnl_get_endl(char *buff, int fd)
-{
-	char	*add;
-	char	*temp;
-	int		rread;
-
-	if (!buff)
-		return (NULL);
-	while (!ft_strchr(buff, '\n'))
-	{
-		temp = buff;
-		add = ft_calloc(BUFFER_SIZE + 1, sizeof (char));
-		rread = read(fd, add, BUFFER_SIZE);
-		if (rread <= 0)
-		{
-			if (!*buff)
-				return (free(temp), free(add), NULL);
-			return (free(add), buff);
-		}
-		buff = ft_strljoin(temp, add, rread);
-		free(temp);
-		free(add);
-	}
-	return (buff);
+	ft_bzero(buff, size);
+	return (read(fd, buff, size));
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buff = NULL;
+	static char	buff[1024][BUFFER_SIZE + 1] = {""};
 	char		*temp;
 	char		*result;
+	int			rread;
 
+	result = NULL;
+	rread = BUFFER_SIZE;
 	if (fd <= 2 && fd)
 		return (NULL);
-	if (!buff)
-		buff = ft_calloc(BUFFER_SIZE + 1, sizeof (char));
-	buff = gnl_get_endl(buff, fd);
-	if (!buff)
-		return (NULL);
-	temp = buff;
-	if (!ft_strchr(buff, '\n'))
+	result = ft_strljoin(result, buff[fd], ft_strlen(buff[fd]));
+	while (((!ft_strchr(result, '\n') && rread > 0)))
 	{
-		result = ft_strdup(buff);
-		buff = NULL;
-		return (free(temp), result);
+		temp = result;
+		rread = ft_read_trunc(fd, buff[fd], BUFFER_SIZE);
+		result = ft_strljoin(result, buff[fd], ft_strlen(buff[fd]));
+		free(temp);
 	}
-	result = ft_substr(buff, 0, ft_strchr(buff, '\n') - buff + 1);
-	buff = ft_strdup(ft_strchr(temp, '\n') + 1);
-	return (free(temp), result);
+	temp = result;
+	result = ft_substr(result, 0, ft_strchr(result, '\n') - result + 1);
+	if (ft_strchr(result, '\n'))
+		ft_memmove(buff[fd], ft_strchr(result, '\n') + 1,
+			ft_strlen(ft_strchr(result, '\n')));
+	if (free(temp), !(*result))
+		return (free(result), NULL);
+	return (result);
 }
--- libft/src/get_next_line_utils.c
+++ libft/src/get_next_line_utils.c
@@ -6,10 +6,17 @@
 /*   By: rastie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 17:11:11 by rastie            #+#    #+#             */
-/*   Updated: 2023/01/10 13:30:59 by rastie           ###   ########.fr       */
+/*   Updated: 2023/06/18 17:17:11 by rastie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"get_next_line.h"
+
+int	min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
 
 char	*ft_strljoin(char const *s1, char const *s2, int size)
 {
@@ -18,7 +25,11 @@ char	*ft_strljoin(char const *s1, char const *s2, int size)
 	int		len_s2;
 
 	len_s1 = 0;
-	if (!size)
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2 || !size)
 		return (ft_strdup(s1));
 	while (s1[len_s1])
 		len_s1++;
@@ -26,10 +37,9 @@ char	*ft_strljoin(char const *s1, char const *s2, int size)
 	while (s2[len_s2])
 		len_s2++;
 	result = malloc(len_s1 + min(size, len_s2) + 1);
-	if (result)
-	{
-		ft_strlcpy(result, s1, len_s1 + 1);
-		ft_strlcpy(result + len_s1, s2, min(size, len_s2) + 1);
-	}
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, s1, len_s1 + 1);
+	ft_strlcpy(result + len_s1, s2, min(size, len_s2) + 1);
 	return (result);
 }
